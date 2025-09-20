f697765dfc0cc858242cdbbaf164f936eb7c2f34
Oleksandr Holopapa <sasha.klever12@gmail.com>
2025-09-20 09:20:08 +0300

    Changed classes: -added field private static final long serialVersionUID = 1L; to Servlets classes,because when executing the mvn clean install command, there was a warning from the compiler regarding the use of serialization -fixed a typo in the variable name massage to message

--- src/main/java/com/javarush/servlets/FailServlet.java
+++ src/main/java/com/javarush/servlets/FailServlet.java
@@ -6,13 +6,13 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.io.Serial;
 
 @WebServlet("/fail")
 public class FailServlet extends HttpServlet {
+    @Serial
+    private static final long serialVersionUID = 1L;
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.getRequestDispatcher("/WEB-INF/jsp/fail.jsp").forward(req, resp);
--- src/main/java/com/javarush/servlets/MountainsServlet.java
+++ src/main/java/com/javarush/servlets/MountainsServlet.java
@@ -9,18 +9,21 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.Serial;
 import java.util.Map;
 
 @WebServlet("/mountains")
 public class MountainsServlet extends HttpServlet {
+    @Serial
+    private static final long serialVersionUID = 1L;
     private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String jsonPath = getServletContext().getRealPath("/resources/json/mountains.json");
-        Map<String, String> jsonData = objectMapper.readValue(new File(jsonPath), Map.class);
+        var jsonData = objectMapper.readValue(new File(jsonPath), Map.class);
 
-        req.setAttribute("massage", jsonData.get("massage"));
+        req.setAttribute("message", jsonData.get("message"));
         req.setAttribute("rightAnswer", jsonData.get("rightAnswer"));
         req.setAttribute("wrongAnswer", jsonData.get("wrongAnswer"));
         req.setAttribute("consequencesOfWrongChoices", jsonData.get("consequencesOfWrongChoices"));
