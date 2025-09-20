37eb0ff4bf89948b9a3e482459f40be5f76c3bd2
Oleksandr Holopapa <sasha.klever12@gmail.com>
2025-09-20 08:34:54 +0300

    changed servlets: -MountainsServlet changed doPost -FailServlet changed doPost

--- src/main/java/com/javarush/servlets/FailServlet.java
+++ src/main/java/com/javarush/servlets/FailServlet.java
@@ -13,11 +13,8 @@ import java.util.Map;
 
 @WebServlet("/fail")
 public class FailServlet extends HttpServlet {
-    Map<String, File> map = new HashMap<>();
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String answer = req.getParameter("answer");
-        req.setAttribute("answer", answer);
         req.getRequestDispatcher("/WEB-INF/jsp/fail.jsp").forward(req, resp);
     }
 }
--- src/main/java/com/javarush/servlets/MountainsServlet.java
+++ src/main/java/com/javarush/servlets/MountainsServlet.java
@@ -1,22 +1,29 @@
 package com.javarush.servlets;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.Map;
 
 @WebServlet("/mountains")
 public class MountainsServlet extends HttpServlet {
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        HttpSession reqSession = req.getSession();
-        reqSession.setAttribute("Message", "There will be my message");
-        reqSession.setAttribute("rightAnswer", "There will be my right answer");
-        reqSession.setAttribute("wrongAnswer", "There will be my wrong answer");
+        String jsonPath = getServletContext().getRealPath("/resources/json/mountains.json");
+        Map<String, String> jsonData = objectMapper.readValue(new File(jsonPath), Map.class);
+
+        req.setAttribute("massage", jsonData.get("massage"));
+        req.setAttribute("rightAnswer", jsonData.get("rightAnswer"));
+        req.setAttribute("wrongAnswer", jsonData.get("wrongAnswer"));
+        req.setAttribute("consequencesOfWrongChoices", jsonData.get("consequencesOfWrongChoices"));
 
         req.getRequestDispatcher("/WEB-INF/jsp/mountains.jsp").forward(req, resp);
     }
