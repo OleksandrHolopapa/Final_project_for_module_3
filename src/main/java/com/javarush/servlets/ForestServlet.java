833b9ca51c0b478b98b298f023a5dfbcd88ea717
Oleksandr Holopapa <sasha.klever12@gmail.com>
2025-09-20 12:55:19 +0300

    Created everything needed for the Forest location: -created ForestServlet -created forest.jsp -created forestStyle.css -created forest.json -created forest.jpg

---
+++ src/main/java/com/javarush/servlets/ForestServlet.java
@@ -0,0 +1,22 @@
+package com.javarush.servlets;
+
+import com.javarush.utils.ServletService;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.Serial;
+
+@WebServlet("/forest")
+public class ForestServlet extends HttpServlet {
+    @Serial
+    private static final long serialVersionUID = 1L;
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String jsonPath = getServletContext().getRealPath("/resources/json/forest.json");
+        ServletService.forward(jsonPath, "/WEB-INF/jsp/forest.jsp", req, resp);
+    }
+}
