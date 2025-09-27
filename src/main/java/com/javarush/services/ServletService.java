76ff1d41fafbe59a525600d143511a211aa06a98
Oleksandr Holopapa <sasha.klever12@gmail.com>
2025-09-27 23:00:55 +0300

    Changed class ServletService: - added logger - added error handling - changed returned type from void to boolean in method setRequestAttributesFromJson

--- src/main/java/com/javarush/services/ServletService.java
+++ src/main/java/com/javarush/services/ServletService.java
@@ -5,23 +5,37 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 import jakarta.servlet.ServletContext;
 import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Collections;
 import java.util.Map;
 
 public class ServletService {
-    private static final ObjectMapper jsonMapper = new JsonMapper();
+    private final ObjectMapper jsonMapper = new JsonMapper();
+    private final Logger logger = LoggerFactory.getLogger(ServletService.class);
 
-    private Map<String, String> getJsonData(ServletContext context, String jsonFilePath) throws IOException {
-        String jsonPath = context.getRealPath(jsonFilePath);
-        return jsonMapper.readValue(new File(jsonPath), new TypeReference<>() {});
+    private Map<String, String> getJsonData(ServletContext context, String jsonFilePath) {
+        try {
+            String jsonPath = context.getRealPath(jsonFilePath);
+            return jsonMapper.readValue(new File(jsonPath), new TypeReference<>() {});
+        } catch (IOException e) {
+            logger.error("Failed to read JSON file: {}", e.getMessage());
+            return Collections.emptyMap();
+        }
     }
 
-    public void setRequestAttributesFromJson(HttpServletRequest request, String jsonFilePath, ServletContext servletContext) throws IOException {
+    public boolean setRequestAttributesFromJson(HttpServletRequest request, String jsonFilePath, ServletContext servletContext) {
         Map<String, String> jsonData = getJsonData(servletContext, jsonFilePath);
-        for (Map.Entry<String, String> entry : jsonData.entrySet()) {
-            request.setAttribute(entry.getKey(), entry.getValue());
+
+        if (jsonData.isEmpty()) {
+            logger.error("JSON data is empty");
+            return false;
         }
+
+        jsonData.forEach(request::setAttribute);
+        return true;
     }
 }
