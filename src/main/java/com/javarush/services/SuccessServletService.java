04762f357f145a985cc00b101850e36534bf1c80
Oleksandr Holopapa <sasha.klever12@gmail.com>
2025-09-28 00:50:34 +0300

    Changed class SuccessServletService - added logger - added error handling

--- src/main/java/com/javarush/services/SuccessServletService.java
+++ src/main/java/com/javarush/services/SuccessServletService.java
@@ -1,18 +1,29 @@
 package com.javarush.services;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
 import java.util.Random;
 
 public class SuccessServletService {
+    private final Logger logger = LoggerFactory.getLogger(SuccessServletService.class);
     private final Random random = new Random();
 
-    public void testYourLuck(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        if (isLuckOnYourSide()) req.getRequestDispatcher("/WEB-INF/jsp/success.jsp").forward(req, resp);
-        else resp.sendRedirect(req.getContextPath()+"/fail?answer="+req.getAttribute("failureMessage"));
+    public void testYourLuck(HttpServletRequest req, HttpServletResponse resp) {
+        try {
+            if (isLuckOnYourSide()) {
+                logger.info("isLuckOnYourSide() returns true");
+                req.getRequestDispatcher("/WEB-INF/jsp/success.jsp").forward(req, resp);
+            } else {
+                logger.info("isLuckOnYourSide() returns false");
+                resp.sendRedirect(req.getContextPath() + "/fail?answer=" + req.getAttribute("failureMessage"));
+            }
+        } catch (Exception e) {
+            logger.error("Error processing luck check request: {}", e.getMessage());
+            throw new RuntimeException(e);
+        }
     }
 
     private boolean isLuckOnYourSide() {
