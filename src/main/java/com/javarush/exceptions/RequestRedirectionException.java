6d0644b6d859ccd9e067903ab7e9627e36f2fd80
Oleksandr Holopapa <sasha.klever12@gmail.com>
2025-10-01 14:46:56 +0300

    Changed RequestRedirectionException: - added: private static final long serialVersionUID = 1L; because without it there is the compiler warning: "serializable class com.javarush.exceptions.RequestRedirectionException has no definition of serialVersionUID"

--- src/main/java/com/javarush/exceptions/RequestRedirectionException.java
+++ src/main/java/com/javarush/exceptions/RequestRedirectionException.java
@@ -1,6 +1,8 @@
 package com.javarush.exceptions;
 
 public class RequestRedirectionException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
     public RequestRedirectionException(String message) {
         super("Request Redirection Exception: "+message);
     }
